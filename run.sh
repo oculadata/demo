29a62df1362617650ada144bc70eb03e55ee716c
Yuantai Du <yuantai.du@aigios.com>
2020-05-29 14:54:19 -0400

    parameterize

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem ec2-user@18.222.18.219 bash -c '~/bin/DemoDaemon.sh'
+DAEMON="${1:-DemoDaemon}.sh"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem ec2-user@18.222.18.219 bash -c "~/bin/$DAEMON"
