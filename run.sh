8f103225196fb53f8beff0c8243f158f31c23d39
Yuantai Du <yuantai.du@aigios.com>
2020-07-10 14:25:09 -0400

    changing to auto scaling

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 DAEMON="${1:-DemoDaemon}.sh"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem ec2-user@18.222.18.219 bash -c "~/bin/$DAEMON"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem ec2-user@3.133.134.32 bash -c "~/bin/$DAEMON"
