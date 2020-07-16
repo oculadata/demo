08138b2fcf0e83ca0ec29466c136bced1df38c49
Yuantai Du <yuantai.du@aigios.com>
2020-07-16 13:05:26 -0400

    update to airflow server

--- scripts/airflow_update.sh
+++ scripts/airflow_update.sh
@@ -1,5 +1,8 @@
-test -d airflow/dags || echo "Can't find folder for DAGs" >&2
+DAGS_DIR=airflow/dags
+test -d $DAGS_DIR || echo "Can't find folder for DAGs" >&2
 
-scp -i ~/.ssh/AigiosAlphaServer.pem airflow/dags/* airflow@airflow.oculadata.com:airflow/dags/
+for f in $DAGS_DIR/*.py ; do
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem $f airflow@airflow.oculadata.com:airflow/dags/
+done
 
 
