29d3bda8c3e76ae76ee6065b9b58246057b4d419
Yuantai Du <yuantai.du@aigios.com>
2020-07-23 14:58:35 -0400

    remove the argument

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 DAEMON="${1:-DemoDaemon}.sh"
 shift
 PARAMS="$*"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem airflow@airflow.oculadata.com bash -c "~/bin/$DAEMON $PARAMS"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/AigiosAlphaServer.pem airflow@airflow.oculadata.com bash "~/bin/$DAEMON $PARAMS"
